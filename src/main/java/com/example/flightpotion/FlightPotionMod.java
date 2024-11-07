208dce6353998d9b0883d7a400dc0f7324e42e50
notkijani <jansen.blaine@gmail.com>
2024-11-07 17:07:55 -0500

    Update FlightPotionMod.java

--- src/main/java/com/example/flightpotion/FlightPotionMod.java
+++ src/main/java/com/example/flightpotion/FlightPotionMod.java
@@ -1,15 +1,40 @@
 package com.example.flightpotion;
 
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.item.Items;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.Potions;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.registry.FabricBrewingRecipeRegistry;
 
 public class FlightPotionMod implements ModInitializer {
+    public static final String MOD_ID = "flightpotion";
+    
+    // Register the effect
+    public static final FlightEffect FLIGHT_EFFECT = Registry.register(
+        Registries.STATUS_EFFECT,
+        new Identifier(MOD_ID, "flight"),
+        new FlightEffect()
+    );
+    
+    // Create and register the potion
+    public static final Potion FLIGHT_POTION = Registry.register(
+        Registries.POTION,
+        new Identifier(MOD_ID, "flight_potion"),
+        new Potion(new StatusEffectInstance(FLIGHT_EFFECT, 20 * 120)) // 120 seconds
+    );
 
     @Override
     public void onInitialize() {
-        // Register the potion effect and potion item
-        ModPotions.registerPotions();
-        
-        // Register brewing recipe
-        ModBrewingRecipes.registerBrewingRecipes();
+        // Register the brewing recipe (Awkward + Feather = Flight Potion)
+        FabricBrewingRecipeRegistry.registerPotionRecipe(
+            Potions.AWKWARD,
+            Items.FEATHER,
+            FLIGHT_POTION
+        );
     }
 }
